74fc6d32a311b927951fb81490e5591b338a8d5d
atlj <cccswagboyyolo@yahoo.com>
2021-05-09 20:38:01 +0300

    style: minor fix

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -1,6 +1,11 @@
 import cn from "classnames";
 
-export const Container = ({ children, className }) => {
+interface Props {
+    children: any;
+    className?: String;
+}
+
+export const Container = ({ children, className }: Props) => {
     return (
         <div className={cn("min-h-screen flex flex-col", className)}>
             {children}
